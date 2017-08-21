9977dae4c739ebfeafa2f3ee2ba51f251913a225
Grey <kajtorvaldgrey@googlemail.com>
2017-08-21 17:01:08 +0200

    tried to implement yuuta's solution

--- lua/autorun/server/sv_end_round_music.lua
+++ lua/autorun/server/sv_end_round_music.lua
@@ -1,65 +1,14 @@
-END_OF_ROUND_SOUNDS = {
-	"round/sound1.mp3",
-	"round/sound2.mp3",
-	"round/sound3.mp3",
-	"round/sound4.mp3",
-	"round/sound5.mp3",
-	"round/sound6.mp3",
-	"round/sound7.mp3",
-	"round/sound8.mp3",
-	"round/sound9.mp3",
-	"round/sound10.mp3",
-	"round/sound11.mp3",
-	"round/sound12.mp3",
-	"round/sound13.mp3",
-	"round/sound14.mp3",
-	"round/sound15.mp3",
-	"round/sound16.mp3",
-	"round/sound17.mp3",
-	"round/sound18.mp3",
-	"round/sound19.mp3",
-	"round/sound20.mp3",
-	"round/sound21.mp3",
-	"round/sound22.mp3",
-	"round/sound23.mp3",
-	"round/sound24.mp3",
-	"round/sound25.mp3",
-	"round/sound26.mp3",
-	"round/sound27.mp3",
-	"round/sound28.mp3",
-	"round/sound29.mp3",
-	"round/sound30.mp3",
-	"round/sound31.mp3",
-	"round/sound32.mp3",
-	"round/sound33.mp3",
-	"round/sound34.mp3",
-	"round/sound35.mp3",
-	"round/sound36.mp3",
-	"round/sound37.mp3",
-	"round/sound38.mp3",
-	"round/sound39.mp3",
-	"round/sound40.mp3",
-	"round/sound41.mp3",
-	"round/sound42.mp3",
-	"round/sound43.mp3",
-	"round/sound44.mp3",
-	"round/sound45.mp3",
-	"round/sound46.mp3",
-	"round/sound47.mp3",
-	"round/sound48.mp3",
-	"round/sound49.mp3",
-	"round/sound50.mp3",
-	"round/sound51.mp3",
-	"round/sound52.mp3",
-	"round/sound53.mp3",
-	"round/sound54.mp3",
-	"round/sound55.mp3",
+-- Enter the amount of sound files here.
+-- Make sure that the sound files are in the round folder
+AMOUNT_OF_FILES = 55
 
-	}
-
--- TODO
--- 1st check how many music files are in round/ and assign every sound variable its number
--- 2nd percentage impleme
+-- Sets the chance of the song to appear
+-- 0 = only chance to appear
+-- sound_chance < 0 = song will not appear
+sound_chance = {}
+for i=0, (AMOUNT_OF_FILES-1) do
+	sound_chance[i] = 0
+end
 
 -- IDEA
 -- 1. declare every song number as 0
@@ -68,27 +17,8 @@ END_OF_ROUND_SOUNDS = {
 -- 4. if they are, increase them by 1 every round
 -- 5. only songs with number 0 can be played
 
-
-	-- prov. sound files
-sound1 = "round/sound1.mp3"
-sound2 = "round/sound2.mp3"
-sound3 = "round/sound3.mp3"
-sound4 = "round/sound4.mp3"
-sound5 = "round/sound5.mp3"
-sound6 = "round/sound6.mp3"
-sound7 = "round/sound7.mp3"
-sound8 = "round/sound8.mp3"
-
-	-- prov. percentage
-sound1_percentage = 100
-sound2_percentage = 100
-sound3_percentage = 100
-sound4_percentage = 100
-sound5_percentage = 100
-sound6_percentage = 100
-sound7_percentage = 100
-sound8_percentage = 100
-
+-- TODO
+-- Test on server
 
 // You must define a default sound to play if some of your lists above are empty.
 END_OF_ROUND_WIN_DEFAULT_SOUND = "round/sound1.mp3";
@@ -96,26 +26,21 @@ END_OF_ROUND_WIN_DEFAULT_SOUND = "round/sound1.mp3";
 if ( SERVER ) then
 	util.AddNetworkString( "_ttt_end_round_music" );
 
-	for k, v in pairs( END_OF_ROUND_SOUNDS ) do resource.AddFile( "sound/" .. v ); end
+	for i=1, AMOUNT_OF_FILES do
+		resource.AddFile( "sound/round/sound"..i ..".mp3");
+	end
 
 	local function endofround( wintype )
 		// Default sound
 		local _sound = END_OF_ROUND_WIN_DEFAULT_SOUND
 
 		if wintype == WIN_INNOCENT or wintype == WIN_TRAITOR or wintype == WIN_TIMELIMIT then
-			_sound = table.Random( END_OF_ROUND_SOUNDS );
+			local selectedSound = math.random(0, AMOUNT_OF_FILES-1);
+			if sound_chance[selectedSound]<0 then
+				sound_chance[selectedSound] = sound_chance[selectedSound] + 1;
+			else
+				_sound = "round/sound"..sound_chance[selectedSound]..".mp3";
 		end
-
-		-- if wintype == WIN_INNOCENT then
-		--	_sound = table.Random( END_OF_ROUND_SOUNDS );
-		-- elseif wintype == WIN_TRAITOR then
-		--	_sound = table.Random( END_OF_ROUND_SOUNDS );
-		-- elseif wintype == WIN_TIMELIMIT then
-		--	_sound = table.Random( END_OF_ROUND_SOUNDS );
-		-- end
-
-
-
 
 -- end of server shit
 		net.Start( "_ttt_end_round_music" );
